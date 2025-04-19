2f3c57da554823bcde17175eff5d94572f9147ff
YashJambure007 <jambureyash2003@gmail.com>
2025-04-19 21:10:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://registration-mern-api-sage.vercel.app"],
+    origin: ["https://registration-mern-aoqi.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
